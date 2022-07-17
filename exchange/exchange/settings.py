22f4fce838f969554e9dd7467ceb245c2474c839
AtleksandrasT <Aleksandras.tubis@gmail.com>
2022-07-17 15:48:36 +0300

    secret_key

--- exchange/exchange/settings.py
+++ exchange/exchange/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from . import local_settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tn^%2jjavcb6jpvmd=nxlg+pd*ck*#_%ql%_b$ll--sdk8b07v'
+SECRET_KEY = local_settings.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
