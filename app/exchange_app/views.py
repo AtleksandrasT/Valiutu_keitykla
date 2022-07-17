feabc02c8baed1b9de50c4cf7ef91419e230b8ec
AtleksandrasT <Aleksandras.tubis@gmail.com>
2022-07-17 22:51:57 +0300

    frontas

---
+++ app/exchange_app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+import requests
+
+
+def exchange(request):
+    response = requests.get(url='https://api.exchangerate-api.com/v4/latest/EUR').json()
+    currencies = response.get('rates')
+    
+    if request.method == 'GET':
+        
+        context = {
+            'currencies': currencies
+        }
+
+        return render(request=request, template_name='exchange_app/index.html', context=context)
